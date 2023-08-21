71fa4667a1f76f2615bdb1cfa6563095d8ffb475
OveDev <ovieigere@gmail.com>
2023-08-21 01:27:17 +0100

    initial commit

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -21,86 +21,79 @@ const CTA = () => {
   }, []);
 
   return (
-   <>
-      <div className="cta section__padding" id="service" >
-        <div className="cta-content" data-aos="fade-up" data-aos-anchor-placement="top-center">
-         <h1>Discover the Significance of Our Online Shoe Destination</h1>
-         <p>Experience the Best of Both Worlds: Seamlessly Explore Our Website and App for Effortless and Stylish Footwear Shopping.</p>
-        </div>
+    <div className="cta section__padding" id="service" >
+      <div className="cta-content" data-aos="fade-up" data-aos-anchor-placement="top-center">
+        <h1>Discover the Significance of Our Online Shoe Destination</h1>
+        <p>Experience the Best of Both Worlds: Seamlessly Explore Our Website and App for Effortless and Stylish Footwear Shopping.</p>
       </div>
 
-      <div className="cta__section section__margin">
-        {<img src={cta1} className="cta1" alt="cta1" data-aos="fade-right"  />}
+      <div className="cta__section">
+       {<img src={cta1} alt="cta1" data-aos="fade-right"  />}
 
         <div className='cta__text'>
-          <h6>A Plethora of Choices Await</h6>
+          <h1>A Plethora of Choices Await</h1>
           <p> Gain access to a vast array of shoes from various brands and styles, ensuring that you can find the perfect pair to suit your taste and needs.</p>
              
           <div className="cta__button">
-            <button className='button3' type="button">Explore Now</button>
+           <button type="button">Explore Now</button>
           </div>
         </div>
 
       </div>
-
-
-      <div className="cta__section section__margin">
-
-        <div className='cta__section1'>
-          <div className='cta__text'>
-            <h6>Personalized Recommendations</h6>
-            <p> Benefit from personalized shoe recommendations based on your browsing history, preferences, and style, helping you discover new and exciting options tailored to your taste.</p>
-             
-            <div className="cta__button">
-              <button className='button3' type="button">Explore Now</button>
-            </div>
-
-          </div>
-
-          {<img src={cta2} className="cta2" alt="cta2" data-aos="fade-left" />}
-
-        </div>
-
-      </div>
-
-
-      <div className="cta__section section__margin">
-        {<img src={cta3} className="cta1" alt="cta3" data-aos="fade-right"  />}
-
+      <div className="cta__section1">
         <div className='cta__text'>
-          <h6>Product Reviews and Ratings</h6>
-          <p>Make informed purchase decisions by accessing genuine customer reviews and ratings for each shoe, giving you insights into the quality, fit, and comfort before making your selection.</p>
+          <h1>Personalized Recommendations</h1>
+          <p>Experience the Best of Both Worlds: Seamlessly Explore Our Website and App for Effortless and Stylish Footwear Shopping.</p>
              
           <div className="cta__button">
-            <button className='button3' type="button">Make informed choices</button>
+           <button type="button">Explore Now</button>
           </div>
         </div>
 
+        {<img src={cta2} className="cta2" alt="cta2" data-aos="fade-left" />}
+
+
       </div>
 
-      <div className="cta__section section__margin">
+      <div className="cta__section">
+       {<img src={cta3} className="cta1" alt="cta3" data-aos="fade-right"  />}
 
-        <div className='cta__section1'>
-          <div className='cta__text'>
-            <h6>Unparalleled Convenience</h6>
-            <p>Discover the Boundless Benefits of Our App, Redefining the Way You Shop for Stylish Footwear with Unmatched Convenience.</p>
+        <div className='cta__text'>
+          <h1>A Plethora of Choices Await</h1>
+          <p> Benefit from personalized shoe recommendations based on your browsing history, preferences, and style, helping you discover new and exciting options tailored to your taste.</p>
              
-            <div className="cta__button">
-              <button className='button3' type="button">Shop with ease</button>
-            </div>
-
+          <div className="cta__button">
+           <button type="button">Explore Now</button>
           </div>
-
-          {<img src={cta4} className="cta2" alt="cta4" data-aos="fade-left" />}
-
         </div>
 
       </div>
 
+      <div className="cta__section1">
+        <div className='cta__text'>
+         <h6>Unparalleled Convenience</h6>
+          <p>Discover the Boundless Benefits of Our App, Redefining the Way You Shop for Stylish Footwear with Unmatched Convenience.</p>
+             
+          <div className="cta__button">
+           <button type="button">Explore Now</button>
+          </div>
+        </div>
 
-     
-    </>
+        {<img src={cta4} className="cta2" alt="cta4" data-aos="fade-left" />}
+
+
+      </div>
+
+      
+    </div>
+
+    
+
+   
+
+
   );
+
 }
 
 export default CTA;
